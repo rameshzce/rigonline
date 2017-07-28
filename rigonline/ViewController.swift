04fd8d511894a1fa3641d46534967b44463be81d
rameshkolamala <rameshzce@gmail.com>
2017-07-28 21:04:03 +0530

    initial setup

--- rigonline/ViewController.swift
+++ rigonline/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIWebViewDelegate {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
