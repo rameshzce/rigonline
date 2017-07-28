061b0f08d2c261e7a6b788a755f9fcacde39abfb
rameshkolamala <rameshzce@gmail.com>
2017-07-28 20:13:18 +0530

    Initial Commit

---
+++ rigonline/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rigonline
+//
+//  Created by Ramesh Kolamala on 28/07/17.
+//  Copyright © 2017 tokkalo.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
